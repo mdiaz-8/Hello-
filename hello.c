493a660d22ff9a850781216beba4a81908805394
mdiaz-8 <69877314+mdiaz-8@users.noreply.github.com>
2020-08-24 14:16:30 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* Author: May Diaz
+* Date: 08/18/2020
+*
+* A simple hello world program
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+  printf("May Diaz\n");
+
+  return 0;
+}
